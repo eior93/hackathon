056b4dca005397fcc3e5c7ac8e5e612777ffcca2
Jeanette Miranda <jeanette_miranda@brown.edu>
2013-02-03 12:48:41 -0500

    Adding capturing multiple photos

--- capture_webcam.py
+++ capture_webcam.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python2.7
 import cv
+import time
 
 print "hello world"
 
 cv.NamedWindow("w1", cv.CV_WINDOW_AUTOSIZE)
 
 camera_index = 0
-capture = cv.CaptureFromCAM(camera_index)
+capture = cv.CaptureFromCAM(0) #camera_index)
 
-frame = cv.QueryFrame(capture)
-cv.SaveImage("pic.jpg", frame)
-
-print "do i have a picture??"
+for i in range(0, 10):
+	frame = cv.QueryFrame(capture)
+	cv.SaveImage("pic" + str(i)+ ".jpg", frame)
+	time.sleep(1)
