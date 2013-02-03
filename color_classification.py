dfdb80642381ba98d50476cda26ae3c90076a1b2
Jeanette Miranda <jeanette_miranda@brown.edu>
2013-02-03 13:14:42 -0500

    Adding minimum number of pixels needed

--- color_classification.py
+++ color_classification.py
@@ -36,6 +36,8 @@ def avg_img_color_np(img_mat):
 	b_values = []
 	height = len(img_mat) # height
 	width = len(img_mat[0]) # width
+	num_non_dropped = 0
+	
 	for r in range(0, height):
 		for c in range(0, width):
 			 sum_r += img_mat[r, c][2]
@@ -47,8 +49,11 @@ def avg_img_color_np(img_mat):
 				 r_values.append(img_mat[r, c][2])
 				 g_values.append(img_mat[r, c][1])
 				 b_values.append(img_mat[r, c][0])
-	
-	return (np.median(r_values), np.median(g_values), np.median(b_values))
+				 num_non_dropped = num_non_dropped + 1
+	if num_non_dropped >= 0.2 * height * width:
+		return (np.median(r_values), np.median(g_values), np.median(b_values))
+	else: # return bgd
+		return colors[10]
 
 def avg_img_color(img_in):
 	sum_r = 0
@@ -58,6 +63,9 @@ def avg_img_color(img_in):
 	g_values = []
 	b_values = []
 	img_mat = cv.GetMat(img_in)
+
+	num_non_dropped = 0
+
 	for r in range(0, img_mat.rows):
 		for c in range(0, img_mat.cols):
 			 sum_r += img_mat[r, c][2]
@@ -69,7 +77,8 @@ def avg_img_color(img_in):
 				 r_values.append(img_mat[r, c][2])
 				 g_values.append(img_mat[r, c][1])
 				 b_values.append(img_mat[r, c][0])
-	
-	num_pix = img_in.width * img_in.height
-
-	return (np.median(r_values), np.median(g_values), np.median(b_values))
+				 num_non_dropped = num_non_dropped + 1
+	if num_non_dropped >= 0.7 * img_in.height * img_in.width:
+		return (np.median(r_values), np.median(g_values), np.median(b_values))
+	else: # return bgd
+		return colors[10]
